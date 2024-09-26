f05753403a3c8974c837fe924bdd1dc71f416fb3
ramoncodevale <ramonvalentim88@gmail.com>
2024-09-26 20:23:28 -0300

    feat: adding the beginning to the portfolio

--- components/sidebar-sheet.tsx
+++ components/sidebar-sheet.tsx
@@ -1,7 +1,6 @@
 import { SheetContent } from "./ui/sheet";
 import Link from 'next/link';
-import { Button } from "./ui/button";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
 
 const SidebarSheet = () => {
     return (
@@ -10,9 +9,6 @@ const SidebarSheet = () => {
                 <div className="flex flex-col items-center gap-3 mt-10">
                     <Link className="text-lg border-b border-text-primary pb-1" href="/" passHref>
                         Início
-                    </Link>
-                    <Link className="text-lg border-b border-text-primary pb-1" href="/sobre-mim" passHref>
-                        Sobre Mim
                     </Link>
                     <Link className="text-lg border-b border-text-primary pb-1" href="/habilidades" passHref>
                         Habilidades
@@ -23,29 +19,13 @@ const SidebarSheet = () => {
                     <Link className="text-lg border-b border-text-primary pb-1" href="/certificados" passHref>
                         Certificados
                     </Link>
-                    <Link className="text-lg border-b border-text-primary pb-1" href="/contatos" passHref>
+                    <Link className="text-lg border-b border-text-primary pb-1" href="/certificados" passHref>
                         Contatos
                     </Link>
+                    
                     <div className="border border-b" />
-
-                 
                 </div>
             </div>
-
-            <Link href="/CV-RAMON-VALENTIM-DA-SILVA-FRONT-END.pdf" target="_blank" rel="noopener noreferrer">
-                <Button className="w-full rounded-lg font-bold text-sm mt-7 p-5">
-                     Visualizar CV    
-                </Button>
-            </Link>
-
-            <Link href="https://github.com/ramoncodevale" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline" className="w-full rounded-lg font-bold text-sm mt-7 p-5">
-                <div className="flex items-center gap-2">
-                GitHub
-                <GitHubLogoIcon />
-                </div>
-                </Button>
-            </Link>
         </SheetContent>
     );
 }
