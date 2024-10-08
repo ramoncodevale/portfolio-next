7a927a93b847e995b409bdf995d853cf654f682f
ramoncodevale <ramonvalentim88@gmail.com>
2024-10-08 18:33:48 -0300

    feat: add progress within the Skills component

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,7 +1,8 @@
 import { Card, CardContent, CardDescription } from "@/components/ui/card";
-import { categorias, habilidades,  } from "../constants";
 import { icons } from "../constants/icons";
-import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
+import { habilidades } from "../constants/habilidades";
+import { categorias } from "../constants/categorias";
 
 const Skills = () => {
   return (
@@ -15,14 +16,16 @@ const Skills = () => {
                 (item) => item.nome === habilidade
               );
               const nivel = habilidadeCategoria ? habilidadeCategoria.nivel : "Desconhecido"; 
-              
+
               const Icon = icons[habilidade.toLowerCase()];
+              const progressValue = nivel === "Avançado" ? 80 : nivel === "Intermediário" ? 60 : 40;
+
               return (
                 <Card key={index} className="bg-card">
                   <CardContent className="flex flex-col items-center mt-5 rounded-lg shadow-xl">
                     {Icon && <Icon className="w-20 h-20 text-primary" />}
                     <CardDescription className="font-bold mt-5">{habilidade}</CardDescription>
-                    <Badge className="text-sm  px-3 py-1 rounded-xl  font-semibold mt-2">{nivel}</Badge>
+                    <Progress value={progressValue} className="w-full mt-3" />
                   </CardContent>
                 </Card>
               );
